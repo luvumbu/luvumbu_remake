5f648053d244ef8eb6b4fe17919ecb195e1d531b
luvumbu <luvumbu.n@gmail.com>
2025-04-30 09:45:25 +0200

    remove element et add sous condition ok

--- all_projet_img/_.php
+++
@@ -1,5 +0,0 @@
-<?php
-$row_projet = 
-array(
-);
-?>
--- req_sql/require_once.php
+++ req_sql/require_once.php
@@ -8,4 +8,54 @@ require_once "../Class/extraireAlphabetique.php" ;
 require_once "../Class/replace_element.php";
 date_default_timezone_set('Europe/Paris');
  
+
+
+$chemin ="../all_projet" ; 
+
+if (!is_dir($chemin)) {
+    mkdir($chemin, 0777, true); // true pour créer récursivement
+    echo "Le dossier a été créé.";
+} 
+
+
+
+
+$chemin ="../all_projet_img" ; 
+
+if (!is_dir($chemin)) {
+    mkdir($chemin, 0777, true); // true pour créer récursivement
+    echo "Le dossier a été créé.";
+} 
+
+
+
+$chemin ="../all_projet_img_json" ; 
+
+if (!is_dir($chemin)) {
+    mkdir($chemin, 0777, true); // true pour créer récursivement
+    echo "Le dossier a été créé.";
+} 
+
+
+
+$chemin ="../all_style" ; 
+
+if (!is_dir($chemin)) {
+    mkdir($chemin, 0777, true); // true pour créer récursivement
+    echo "Le dossier a été créé.";
+} 
+
+
+$chemin ="../qr_code_1/temp" ; 
+
+if (!is_dir($chemin)) {
+    mkdir($chemin, 0777, true); // true pour créer récursivement
+    echo "Le dossier a été créé.";
+} 
+
+
+ 
+
+
+
 ?>
