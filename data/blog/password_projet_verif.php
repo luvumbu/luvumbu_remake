3734c19f044d5efdf5d940fcfcd5cbe5833d00da
luvumbu <luvumbu.n@gmail.com>
2025-09-01 11:12:54 +0200

    protection de la page par  mor de passe

---
+++ data/blog/password_projet_verif.php
@@ -0,0 +1,89 @@
+ <?php
+
+    if (!isset($_SESSION["password_projet"])) {
+
+
+        $_SESSION["password_projet_ascii"] = $password_projet_x[0];
+
+
+
+    ?>
+
+     <div class="password_input">
+         <input type="password" id="password" placeholder="Entrez le mot de passe de la page">
+         <div class="submit_button" title="<?= $_SESSION["index"][4] ?>" onclick="verifierMotDePasse()">Envoyer</div>
+     </div>
+ <?php
+    } else {
+
+        require_once "data/blog/blog_body.php";
+
+        ?>
+
+<?php 
+    }
+
+    ?>
+
+
+
+
+ <style>
+     .password_input {
+         width: 80%;
+         margin: auto;
+         margin-top: 45px;
+
+         border-radius: 7px;
+     }
+
+     .password_input input {
+         width: 100%;
+         padding: 15px;
+
+     }
+
+     .submit_button {
+         background-color: #4CAF50;
+         color: white;
+         padding: 15px 20px;
+         border: none;
+         border-radius: 5px;
+         cursor: pointer;
+         margin-top: 25px;
+         text-align: center;
+     }
+ </style>
+
+ <script>
+     function verifierMotDePasse(_this) {
+
+
+       
+         var verifierMotDePasse = document.getElementById("password").value;
+
+
+         var ok = new Information("../req_sql/verifierMotDePasse.php"); // création de la classe 
+         ok.add("verifierMotDePasse", verifierMotDePasse); // ajout de l'information pour lenvoi 
+         console.log(ok.info()); // demande l'information dans le tableau
+         ok.push(); // envoie l'information au code pkp 
+
+
+
+
+
+ 
+         const myTimeout = setTimeout(myGreeting, 300);
+
+         function myGreeting() {
+             location.reload();
+         }
+
+    
+
+
+
+
+
+     }
+ </script>
---
+++ req_sql/verifierMotDePasse.php
@@ -0,0 +1,23 @@
+<?php 
+require_once "require_once.php";
+//echo AsciiConverter::asciiToString($_SESSION["password_projet_ascii"]);
+ 
+$ascii_password = AsciiConverter::stringToAscii($_POST["verifierMotDePasse"]);
+$password_projet_ascii = $_SESSION["password_projet_ascii"];
+
+ 
+
+ 
+if($ascii_password === $password_projet_ascii) {
+ 
+
+    $_SESSION["password_projet"] = true;
+} else {
+ 
+    unset($_SESSION["password_projet"]);
+}
+ 
+
+
+
+?>
